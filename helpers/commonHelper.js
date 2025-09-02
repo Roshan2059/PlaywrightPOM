b55593b8dec53a28fec534ba486a27ea1078591e
Roshan Panta <roshanpanta2059@gmail.com>
2025-09-02 11:07:56 +0545

    optimized prompts test

--- helpers/commonHelper.js
+++ helpers/commonHelper.js
@@ -28,7 +28,20 @@ async function performLogin(page, username, password, directedToDashboard = true
     }
 }
 
+async function makeid(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    console.log("result", result);
+    return result;
+}
+
+// console.log(makeid(5));
+
 module.exports = {
     checkDashboardLink,
-    checkLoginLink, performLogin
+    checkLoginLink, performLogin, makeid
 };
